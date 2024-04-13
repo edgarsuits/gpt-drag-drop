f8241f5e92fda70986d25cbf59d71ed09dc0b1c2
edgarsuits <tcalo3@gatech.edu>
2024-04-13 12:44:02 -0400

    heroku demo

--- database_setup/agents.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS agents (
-    id text PRIMARY KEY,
-    tutor_id text NOT NULL,
-    shared_data text NOT NULL,
-    model text NOT NULL,
-    problems text
-);
--- database_setup/tutors.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS tutors (
-    id text PRIMARY KEY,
-    name text NOT NULL,
-    content text NOT NULL,
-    IsOnline integer,
-    follows integer,
-    agent_id text
-);
